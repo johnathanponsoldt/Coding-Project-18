86a693e1c1f98f8932e0cd43fce84374395ec208
johnathanponsoldt <jponsoldt772@gmail.com>
2024-12-08 23:05:21 -0500

    Fetch chart data from JSON file

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,22 @@ function App() {
 }
 
 export default App
+
+const [chartData, setChartData] = useState(null);
+useEffect(() => {
+  fetch('/data.json')
+    .then((response) => response.json())
+    .then((data) => setChartData(data));
+}, []);
+if (!chartData) {
+  return <div>Loading...</div>;
+}
+return (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Dynamic Charts with React and Chart.js</h1>
+    <BarChart data={chartData} />
+    <LineChart data={chartData} />
+    <ScatterChart data={chartData} />
+    <BubbleChart data={chartData} />
+  </div>
+);
